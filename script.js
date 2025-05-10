d927758690d915fc670847f634db3e41404576a9
AngeSilk <61769857+AngeSilk@users.noreply.github.com>
2025-05-10 00:01:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const treeDatabase = {
         certification: "FSC Certificado",
         certificationStatus: "active",
         additionalInfo: "Cosechado bajo pautas sostenibles. Plantación de 12 años.",
-        imageUrl: "https://drive.google.com/file/d/1BCRGOHsICVYV4z-x6j05In4QSiQaCy5y/view?usp=drive_link",
+        imageUrl: "./sources/pino_elliotis.webp",
         imageAlt: "Pino Elliotis"
     },
     "tree-002": {
